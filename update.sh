820677eca7b5a692cb442bcc1214fae23fd1b988
Adam Mischke <mischkeaa@gmail.com>
2023-05-12 15:47:21 -0500

    fix alot

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ cp ../ryzen-hackintosh-dude/ryzen-hackintosh/BOOT/BOOTx64.efi EFI/BOOT/BOOTx64.e
 cp -r ../ryzen-hackintosh-dude/ryzen-hackintosh/OC/Drivers/* EFI/OC/Drivers
 cp -r ../ryzen-hackintosh-dude/ryzen-hackintosh/OC/Kexts/* EFI/OC/Kexts
 cp -r ../ryzen-hackintosh-dude/ryzen-hackintosh/OC/OpenCore.efi EFI/OC/OpenCore.efi
+cp -r ../ryzen-hackintosh-dude/ryzen-hackintosh/OC/Resources EFI/OC
+cp -r ../ryzen-hackintosh-dude/ryzen-hackintosh/OC/Tools EFI/OC
